70f4b4bc2cde2958108ebfda94dde36a78f89669
Salahuddeen-08 <76877263+Salahuddeen-08@users.noreply.github.com>
2021-01-31 20:45:48 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+// variables - containers that stores some value
+// create variable using 3 kywords - let, var, const
+
+let sum;
+sum=10;
+console.log(sum);
+
+// datatypes
+// number - number-15
+// strings - sequence of text = 'ProGrad'
+// booleen - true or false
+// array - store multiple values -[10,20 ,30,'ProGrad','Junior']
+
+var name='Salah'
+console.log(name);
+
+// operations
+// addition
+
+console.log(10+20);
+
+let num1=20;
+let num2=30;
+let addition=num1+num2;
+console.log(addition);
+
+// subtraction
+
+let subtraction=num1-num2;
+console.log(subtraction);
+
+//multiplication
+
+let multiplication=num1*num2;
+console.log(multiplication);
+
+//division
+
+let division=num1/num2;
+console.log(division);
+
+ // add 2 strings
+
+ let name1='ProGrad';
+ let name2='Junior';
+ console.log(name1+" "+name2);
+
